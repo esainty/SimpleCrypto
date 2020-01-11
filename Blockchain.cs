2b26ea26721b8f5a006815fa6807743dc4ce113e
Eden Sainty <eden.sainty@gmail.com>
2020-01-11 18:25:11 +1100

    Merge branch 'master' of https://github.com/esainty/simple-crypto  the commit. Initial merge of README with working code on client.

--- Blockchain.cs
+++ Blockchain.cs
@@ -12,12 +12,12 @@ public class Blockchain {
     }}
     
     public Blockchain() {
-        string genesisHash = generateBlockHash(0, "", 0, "Genesis");
+        string genesisHash = _generateBlockHash(0, "", 0, "Genesis");
         blockchain.Add(new Block(0, "0", genesisHash, 0, "Genesis"));
     }
 
     public bool replaceChain(List<Block> newChain) {
-        if (isValidChain(newChain) && newChain.Count > blockchain.Count) {
+        if (_isValidChain(newChain) && newChain.Count > blockchain.Count) {
             blockchain = newChain;
             return true; 
         } else {
@@ -26,20 +26,20 @@ public class Blockchain {
         }
     }
 
-     private Block generateBlock(string blockData) {
+     private Block _generateBlock(string blockData) {
         Block previousBlock = latestBlock;
         int nextIndex = previousBlock.index + 1;
         int nextTimestamp = _timestamp;
-        string nextHash = generateBlockHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
+        string nextHash = _generateBlockHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
         return new Block(nextIndex, previousBlock.hash, nextHash, nextTimestamp, blockData);
     }
 
-    private string generateBlockHash(int index, string previousHash, int timestamp, string data) {
+    private string _generateBlockHash(int index, string previousHash, int timestamp, string data) {
         string toHash = index + previousHash + timestamp + data;
         return Cryptography.hashStringSHA256(toHash);
     }
 
-    private bool isValidBlock(Block newBlock, Block previousBlock) {
+    private bool _isValidBlock(Block newBlock, Block previousBlock) {
         if (newBlock.index != previousBlock.index - 1) {
             Console.WriteLine("New block has invalid index");
             return false;
@@ -48,18 +48,18 @@ public class Blockchain {
             Console.WriteLine("New block has invalid previous hash");
             return false;
         }
-        if (generateBlockHash(newBlock.index, newBlock.previousHash, newBlock.timestamp, newBlock.data) != newBlock.hash) {
+        if (_generateBlockHash(newBlock.index, newBlock.previousHash, newBlock.timestamp, newBlock.data) != newBlock.hash) {
             Console.WriteLine("New block has invalid hash");
         }
         return true;
     }
 
-    private bool isValidChain(List<Block> chain) {
+    private bool _isValidChain(List<Block> chain) {
         if (!Equals(chain[0], blockchain[0])) {
             return false;
         }
         for (int i = 1; i < chain.Count; i++) {
-            if (!isValidBlock(chain[i], chain[i-1])) {
+            if (!_isValidBlock(chain[i], chain[i-1])) {
                 return false;
             }
         }
